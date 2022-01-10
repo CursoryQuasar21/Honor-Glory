7c3864bfa10755d160f2c53757307bf68df122e9
AlvaroDeVega <alvariito001@gmail.com>
2022-01-10 12:18:36 +0100

    Version_1.01-Inicial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Adrian Diaz">
+    <meta name="author" content="Alvaro De Vega">
+    <title>HomePage</title>
+</head>
+<body>
+    <?php include_once "header/header.php";?>
+    <?php include_once "homepage/homePage.php";?>
+    <?php include_once "footer/footer.php";?>
+</body>
+</html>
